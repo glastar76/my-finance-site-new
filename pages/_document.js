88e2548ace767bf8d7d51d9bfd07b682ddcfad8a
glastar76 <glastar7600@gmail.com>
2025-06-10 19:56:44 -0400

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,7 @@ export default function Document() {
         <meta property="og:title" content="EyeOnFinance - See your money clearly" />
         <meta property="og:description" content="Plan your paycheck, track debt, and reach your savings goals." />
         <meta property="og:image" content="/og-image.png" /> {/* Optional: if you add an Open Graph image */}
-        <meta property="og:url" content="my-finance-site-new.vercel.app"
+        <meta property="og:url" content="my-finance-site-new.vercel.app"/>
  />
       </Head>
       <body>
