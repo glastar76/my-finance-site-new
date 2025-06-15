5b689772769985498046b6436cd63c3cd822adfe
glastar76 <glastar7600@gmail.com>
2025-06-15 19:43:08 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,4 +3,30 @@ import '../styles/globals.css';
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
+  // pages/_app.js
+import Script from 'next/script';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-GNG3TTGSFD');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
+
 }
