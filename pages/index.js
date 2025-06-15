e47bead19e1f5c019396f08322eb51772cac8cec
glastar76 <glastar7600@gmail.com>
2025-06-15 18:16:14 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
         <title>EyeOnFinance - Home</title>
       </Head>
 
-      <Navbar />
+   
 
       <main className="min-h-screen bg-[#0f172a] text-white flex flex-col items-center justify-center px-4 text-center">
         <h1 className="text-4xl font-bold mb-4">Welcome to EyeOnFinance</h1>
