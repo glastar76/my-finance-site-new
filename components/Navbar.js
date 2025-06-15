79e400263226f188e2c25f81ccc79572ffd4b30f
glastar76 <glastar7600@gmail.com>
2025-06-15 18:12:21 -0400

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,24 +1,22 @@
+// components/Navbar.js
 import Link from 'next/link';
 
 export default function Navbar() {
   return (
     <nav className="bg-[#111827] text-white px-6 py-4 shadow-md">
       <div className="max-w-7xl mx-auto flex justify-between items-center">
-        {/* Site Name */}
         <div className="text-2xl font-bold">
           <Link href="/">EyeOnFinance</Link>
         </div>
-
-        {/* Navigation Links */}
         <div className="space-x-6 text-lg">
-          <Link href="/paycheck" className="hover:text-blue-400">
-            Paycheck
+          <Link href="/paycheck" legacyBehavior>
+            <a className="hover:text-blue-400">Paycheck</a>
           </Link>
-          <Link href="/debt-planner" className="hover:text-blue-400">
-            Debt Planner
+          <Link href="/debt-planner" legacyBehavior>
+            <a className="hover:text-blue-400">Debt Planner</a>
           </Link>
-          <Link href="/savings-goals" className="hover:text-blue-400">
-            Savings Goals
+          <Link href="/savings-goals" legacyBehavior>
+            <a className="hover:text-blue-400">Savings Goals</a>
           </Link>
         </div>
       </div>
