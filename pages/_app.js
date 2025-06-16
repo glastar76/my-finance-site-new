19cbd148420ff5a667e884662e8ff52f9d3129bd
glastar76 <glastar7600@gmail.com>
2025-06-16 11:54:20 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,28 +1,39 @@
+// pages/_app.js
 import '../styles/globals.css';
-
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-  // pages/_app.js
+import Head from 'next/head';
 import Script from 'next/script';
 
-<Head>
-  <script 
-  async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2477753161296870"
-     crossorigin="anonymous"></script>
-  <script a
-  sync src={`https://www.googletagmanager.com/gtag/js?id=G-VF040WZY3W`}></script>
-  <script dangerouslySetInnerHTML={{
-    __html: `
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', 'G-VF040WZY3W');
-    `}} />
-</Head>
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        {/* Google AdSense */}
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2477753161296870"
+          crossOrigin="anonymous"
+        ></script>
+
+        {/* Google Analytics */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-VF040WZY3W"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-VF040WZY3W');
+            `,
+          }}
+        />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
   );
 }
 
 export default MyApp;
-
-}
