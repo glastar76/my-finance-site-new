3aa6036680f6618fa09a8ad17c07ba82261c59ca
glastar76 <glastar7600@gmail.com>
2025-06-16 10:01:15 -0400

    Update paycheck.js

--- pages/paycheck.js
+++ pages/paycheck.js
@@ -46,8 +46,7 @@ export default function PaycheckCalculator() {
   return (
     <>
       <Head>
-      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2477753161296870"
-     crossorigin="anonymous"></script>
+
         <title>EyeOnFinance - Paycheck Calculator</title>
       </Head>
       <Navbar />
